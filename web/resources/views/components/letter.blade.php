d9e4174dfe64e6cefa90f2c71566cdd9bff341ac
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-05-15 17:44:31 +0200

    Change letter position in filesystem to add to version control

--- web/resources/views/components/letter.blade.php
+++ web/resources/views/components/letter.blade.php
@@ -6,7 +6,7 @@
 <div class="fbd-letter mt-6 md:mt-8 bg-white drop-shadow-2xl p-6 md:p-8">
     @php
         $locale = app()->getLocale();
-        $letter = file_get_contents(storage_path("app/public/letter.{$locale}.md"));
+        $letter = file_get_contents(public_path("markdown/letter.{$locale}.md"));
         $parser = new Illuminate\Support\Str;
         echo $parser->markdown($letter);
     @endphp
