9568789365b635dbbc0016063d32c3f85290b53f
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-05-19 17:40:31 +0200

    Add Validation translation

--- web/lang/fr/validation.php
+++ web/lang/fr/validation.php
@@ -144,7 +144,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => ':attribute ist bereits in Verwendung.',
+    'unique' => ':attribute est déjà utilisé.',
     'uploaded' => 'The :attribute failed to upload.',
     'uppercase' => 'The :attribute must be uppercase.',
     'url' => 'The :attribute must be a valid URL.',
